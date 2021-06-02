032fb09c2e1790f8ec11436122a1cc0d12ece105
wangbei <45655449+WangBei2018@users.noreply.github.com>
2021-06-02 21:23:15 +0800

    add index and bootstrap

---
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -0,0 +1,18 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/hello")           // 输入时，访问下面的hello函数
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";             // 通过resources/index.html访问页面
+    }
+    @GetMapping("/")
+    public String index(){return "index";}
+
+}
